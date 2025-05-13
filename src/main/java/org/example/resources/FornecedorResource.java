6b208f2e5dac6d067812868c3160412c5afb0a0c
samuelvictor2 <samuelmachado25062007@gmail.com>
2025-05-13 14:57:07 -0300

    atualizando projeto

--- src/main/java/org/example/resources/FornecedorResource.java
+++ src/main/java/org/example/resources/FornecedorResource.java
@@ -1,6 +1,53 @@
 package org.example.resources;
 
-public class FornecedorResource {
+import org.example.entities.Fornecedor;
+import org.example.services.FornecedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping(value = "/fornecedor")
+public class FornecedorResource {
+    @Autowired
+    private FornecedorService fornecedorService;
+
+    @GetMapping
+    public ResponseEntity<List<Fornecedor>> getAll() {
+        List<Fornecedor> funcoes = fornecedorService.getAll();
+        return ResponseEntity.ok(funcoes);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Fornecedor> findById(@PathVariable Long id) {
+        Fornecedor obj = fornecedorService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Fornecedor> insert(@RequestBody Fornecedor fornecedor) {
+        Fornecedor createdFornecedor = fornecedorService.insert(fornecedor);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdFornecedor);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Fornecedor fornecedor) {
+        if (fornecedorService.update(id, fornecedor)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        fornecedorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
